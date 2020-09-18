f5ea105b6cbc6f1117bb063f8a23a1f5fa4d21fb
Adrian Storr <Adrianloves123@gmail.com>
2020-09-18 15:22:35 -0400

    refactoring:tutoring

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,6 +1,6 @@
 // Testing
 // Array of objects
-const quiz = [
+const availableQuestions = [
     {
         q: 'Commonly used data types DO NOT include:',
         options: ['srtings', 'Boolean', 'Alert', 'Numbers'],
@@ -28,9 +28,6 @@ const quiz = [
     },
 ]
 
-
-
-
 const questionNumber = document.querySelector(".question-number");
 const questionText = document.querySelector(".question-text");
 const optionContainer = document.querySelector(".option-container");
@@ -39,159 +36,164 @@ const homeBox = document.querySelector(".home-box");
 const quizBox = document.querySelector(".quiz-box");
 const resultBox = document.querySelector(".result-box");
 
-
-
 let questionCounter = 0;
-let currentQuestion;
-let availableQuestions = [];
+// let currentQuestion;
+// let availableQuestions = [];
 let availableOptions = [];
-let correctAnswers= 0;
+let correctAnswers = 0;
 let attempt = 0;
 
 var totalWorkSeconds = 0;
 var totalRestSeconds = 0;
 var secondsElasped = 0;
 var interval;
+var disable = false;
 
 // push question in availableQuestion Array
-function setAvailableQuestions() {
-    const totalQuestion = quiz.length;
-    for (let i = 0; i < totalQuestion; i++) {
-        availableQuestions.push(quiz[i])
-    }
+// function setAvailableQuestions() {
+//     const totalQuestion = quiz.length;
+//     for (let i = 0; i < totalQuestion; i++) {
+//         availableQuestions.push(quiz[i])
+//     }
 
-}
+// }
 // set question number and options
 function getNewQuestion() {
     // set question number
-    questionNumber.innerHTML = "Question" + (questionCounter + 1) + " of " + quiz.length;
+    questionNumber.textContent = "Question" + (questionCounter + 1) + " of " + availableQuestions.length;
 
     // set question text
     // get random question
-    const questionIndex = availableQuestions[Math.floor(Math.random() * availableQuestions.length)]
-    currentQuestion = questionIndex;
-    questionText.innerHTML = currentQuestion.q;
+    questionText.textContent = availableQuestions[questionCounter].q;
 
     // get the position of 'questionIndex'from the availableQuestion array
-    const index1 = availableQuestions.indexOf(questionIndex);
+    // const index1 = availableQuestions.indexOf(questionIndex);
     // remove the question index from the availableQustion array so that the question does not repeat
-    availableQuestions.splice(index1, 1);
+    // availableQuestions.splice(index1, 1);
 
     // set options
     // get length of options
-    const optionLen = currentQuestion.options.length
+    // const optionLen = currentQuestion.options.length
     // push options  into availableOptions array
-    for (let i = 0; i < optionLen; i++) {
-        availableOptions.push(i)
-    }
+    // for (let i = 0; i < currentQuestion.options.length; i++) {
+    //     availableOptions.push(i)
+    // }
     optionContainer.innerHTML = '';
-    let animationDelay = 0.2;
+    // let animationDelay = 0.2;
     // create option in html
-    for (let i = 0; i < optionLen; i++) {
+    for (let i = 0; i < availableQuestions[questionCounter].options.length; i++) {
         // random option
-        const optionIndex = availableOptions[Math.floor(Math.random() * availableOptions.length)];
+        // const optionIndex = availableOptions[Math.floor(Math.random() * availableOptions.length)];
         // get the position of option index from the availabeOption array
-        const index2 = availableOptions.indexOf(optionIndex);
+        // const index2 = availableOptions.indexOf(optionIndex);
         // remove the optionIndex from the availableOptions so the option do not repeat
-        availableOptions.splice(index2, 1)
+        // availableOptions.splice(index2, 1)
         const option = document.createElement("div");
-        option.innerHTML = currentQuestion.options[optionIndex];
-        option.id = optionIndex;
-        option.style.animationDelay = animationDelay + 's';
-        animationDelay = animationDelay + 0.2;
+        option.textContent = availableQuestions[questionCounter].options[i];
+        // option.id = optionIndex;
+        // option.style.animationDelay = animationDelay + 's';
+        // animationDelay = animationDelay + 0.2;
         option.className = "option";
+        option.addEventListener('click', getResult);
+
         optionContainer.appendChild(option)
-        option.setAttribute("onclick", "getResult(this)");
+        // option.setAttribute("onclick", "getResult(this)");
     }
 
-    questionCounter++
+    // questionCounter++
 
 }
 // get the result of current attempts
-function getResult(element) {
-    const id = parseInt(element.id);
+function getResult() {
+    if (disable) return;
+
+    disable = true;
+    // const id = parseInt(this.id);
     // get answer by comparing click option
 
-    if (id === currentQuestion.answer) {
+    if (availableQuestions[questionCounter].options.indexOf(this.textContent) === availableQuestions[questionCounter].answer) {
         //   set the  green color for correct answer
-        element.classList.add("correct");
-        updateAnswerIndicator("correct");
+        this.classList.add("correct");        
+        answersIndicator("correct");
         correctAnswers++;
-        console.log("correct:" + correctAnswers)
     }
     //   set red color for wrong answer
     else {
-        element.classList.add("wrong");
-         updateAnswerIndicator("wrong")
+        this.classList.add("wrong");
+         answersIndicator("wrong")
     }
-        //   if answer is incorrect then show correct option by adding green color
-        const optionLen = optionContainer.children.length;
-        for (let i = 0; i < optionLen; i++) {
-            if (parseInt(optionContainer.children[i].id === currentQuestion.answer)){
+    //   if answer is incorrect then show correct option by adding green color
+    // const optionLen = optionContainer.children.length;
+    // for (let i = 0; i < optionLen; i++) {
+    //     if (parseInt(optionContainer.children[i].id === currentQuestion.answer)){
 
 
-                optionContainer.children[i].classList.add("correct");
-            }
-        }
-    
-  attempt++;
-    unclickableOptions();
- }
+    //         optionContainer.children[i].classList.add("correct");
+    //     }
+    // }
+
+    //   attempt++;
+    // unclickableOptions();
+}
 // make all options unclickable once user selects option restricting user from making 2 selections
-function unclickableOptions() {
-    const optionLen = optionContainer.children.length;
-    for (let i = 0; i < optionLen; i++) {
-        optionContainer.children[i].classList.add("already-answered");
-    }
-}
-function answersIndicator(){
-      answersIndicatorContainer.innerHTML= '';
-      const totalQuestion = quiz.length;
-    for (let i = 0; i < totalQuestion; i++) {
+// function unclickableOptions() {
+// for (let i = 0; i < optionContainer.children.length; i++) {
+// optionContainer.children[i].removeEventListener("click", getResult)
+// optionContainer.children[i].classList.add("already-answered");
+// }
+// }
+function answersIndicator(type){
+    //   answersIndicatorContainer.innerHTML= '';
+    //   const totalQuestion = quiz.length;
+    // for (let i = 0; i < totalQuestion; i++) {
+        // console.log(answersIndicatorContainer)
+        // console.log(indicator)
         const indicator = document.createElement("div");
-        console.log(answersIndicatorContainer)
-        console.log(indicator)
+        indicator.classList.add(type)
         answersIndicatorContainer.appendChild(indicator);
-    }
+    // }
 }
-function updateAnswerIndicator(markType) {
-    answersIndicatorContainer.children[questionCounter-1].classList.add(markType)
-}
+// function updateAnswerIndicator(markType) {
+//     answersIndicatorContainer.children[questionCounter-1].classList.add(markType)
+// }
 
 function next() {
-    if (questionCounter === quiz.length) {
-        console.log("quiz over");
-        quizOver();
+    disable = false;
+    questionCounter++;
+
+    if (questionCounter === availableQuestions.length) {
+        // console.log("quiz over");
+        quizResult();
+        return
     }
-    else {
-        getNewQuestion();
-    }
+    getNewQuestion();
 }
- function quizOver() {
-//    hide quiz quizBox
-   quizBox.classList.add("hide");
-//    show result box
-   resultBox.classList.remove("hide");
-   quizResult();
- }
+//  function quizOver() {
+//      quizResult();
+//     }
 //  get quiz result
-function quizResult(){
-  resultBox.querySelector(".total-question").innerHTML= quiz.length;
-  resultBox.querySelector(".total-attempt").innerHTML= attempt;
-  resultBox.querySelector(".total-correct").innerHTML= correctAnswers;
-  resultBox.querySelector(".total-wrong").innerHTML= attempt-correctAnswers;
-  resultBox.querySelector(".time")
-  resultBox.querySelector(".total-score").innerHTML= correctAnswers + " / " + quiz.length;
+function quizResult() {
+    //    hide quiz quizBox
+    quizBox.classList.add("hide");
+    //    show result box
+    resultBox.classList.remove("hide");
+
+    resultBox.querySelector(".total-question").innerHTML = availableQuestions.length;
+    resultBox.querySelector(".total-attempt").innerHTML = attempt;
+    resultBox.querySelector(".total-correct").innerHTML = correctAnswers;
+    resultBox.querySelector(".total-wrong").innerHTML = availableQuestions.length - correctAnswers;
+    resultBox.querySelector(".time")
+    resultBox.querySelector(".total-score").innerHTML = correctAnswers + " / " + availableQuestions.length;
 }
 
 function resetQuiz() {
-     questionCounter = 0;
-     correctAnswers= 0;
-     attempt = 0;
-       
+    questionCounter = 0;
+    correctAnswers = 0;
+    attempt = 0;
+
 }
 
-function tryAgainQuiz(){
+function tryAgainQuiz() {
     // hide result box
     resultBox.classList.add("hide");
     // show the quiz box
@@ -207,10 +209,10 @@ function startQuiz() {
     // show quiz box
     quizBox.classList.remove("hide");
     // set all questions in availableQuestions array
-    setAvailableQuestions();
+    // setAvailableQuestions();
     // then call getQuestion()function
     getNewQuestion();
-    answersIndicator();
+    // answersIndicator();
 }
 
 
